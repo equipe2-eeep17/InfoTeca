708d00934145f2c7878358b0f94d31195e7d5b5d
Claudevan <Claudevan@20.20.1.67>
2017-11-09 15:32:46 -0200

    1°comit

---
+++ src/view/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+/**
+ *
+ * @author Claudevan
+ */
+public class Main {
+    public static void main(String[] args) {
+        Tela_login log = new Tela_login();
+        log.setVisible(true);
+        log.setLocationRelativeTo(null);
+    }
+    
+}
